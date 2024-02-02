331a9ce8e9b0376848bebaf5c1426d459efbcdca
Dmitry <neopatogen@gmail.com>
2024-02-02 02:26:29 +0100

    BASE

---
+++ src/types/index.ts
@@ -0,0 +1,26 @@
+/// Types for implementing the base Events class
+export type EventName = string | RegExp;
+export type Subscriber = Function;
+export type EmitterEvent = {
+	eventName: string;
+	data: unknown;
+};
+
+/// Common event methods
+export interface IEvents {
+	on<T extends object>(event: EventName, callback: (data: T) => void): void;
+	emit<T extends object>(event: string, data?: T): void;
+	trigger<T extends object>(
+		event: string,
+		context?: Partial<T>
+	): (data: T) => void;
+}
+
+/// Server response data
+export type ApiListResponse<Type> = {
+	total: number;
+	items: Type[];
+};
+
+/// Server request methods
+export type ApiPostMethods = 'POST' | 'PUT' | 'DELETE';
